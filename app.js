d56e54e4c8843996fe11b829cbe7bb59c4e38cbc
JG-OLIVEIRA <jorge.gdoliveira8@gmail.com>
2021-12-06 00:18:07 -0300

    remove file.jade

--- app.js
+++ app.js
@@ -4,7 +4,6 @@ var path = require("path");
 var cookieParser = require("cookie-parser");
 var logger = require("morgan");
 
-var indexRouter = require("./routes/index");
 var usersRouter = require("./routes/users");
 var booksRouter = require("./routes/book");
 var exchangesRouter = require("./routes/exchanges");
@@ -21,7 +20,6 @@ app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, "public")));
 
-app.use("/", indexRouter);
 app.use("/users", usersRouter);
 app.use("/books", booksRouter);
 app.use("/exchanges", exchangesRouter);
