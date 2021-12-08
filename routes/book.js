a43c03a585df8d6ac228522525d0c049958cea31
JG-OLIVEIRA <jorge.gdoliveira8@gmail.com>
2021-12-08 19:14:22 -0300

    adding all validations

--- routes/book.js
+++ routes/book.js
@@ -2,11 +2,33 @@ const express = require("express");
 const router = express.Router();
 const booksController = require("../controllers/booksController");
 const authorization = require("../middlewares/authorization");
+const { body } = require("express-validator");
+const validator = require("../middlewares/validator");
 
-router.post("/", authorization, booksController.create);
 router.get("/", booksController.readAll);
 router.get("/search", booksController.getByNameOrAuthorOrISBN);
 router.get("/:id", booksController.getById);
+router.post(
+  "/",
+  [
+    body("name")
+      .isLength({ min: 3 })
+      .withMessage("o nome do livro precisa ter pelo menos 3 cacteres"),
+    body("author")
+      .isLength({ min: 3 })
+      .withMessage("o nome do autor precisa ter pelo menos 3 cacteres"),
+    body("isbn")
+      .isNumeric()
+      .isEmpty()
+      .withMessage("o código ISBN precisa ser válido"),
+    body("description")
+      .isLength({ min: 15 })
+      .withMessage("a descrição do livro precisa ter pelo menos 15 caracteres"),
+  ],
+  validator,
+  authorization,
+  booksController.create
+);
 router.put("/:id", authorization, booksController.updateById);
 router.delete("/:id", authorization, booksController.destroyById);
 
--- routes/users.js
+++ routes/users.js
@@ -3,7 +3,7 @@ const router = express.Router();
 const usersController = require("../controllers/usersController");
 const { body } = require("express-validator");
 const authorization = require("../middlewares/authorization");
-const usersValidator = require("../middlewares/usersValidator");
+const validator = require("../middlewares/validator");
 
 router.get("/", usersController.read);
 router.get("/books", authorization, usersController.getAllBooks);
@@ -12,14 +12,14 @@ router.post(
   [
     body("firstName").isLength({ min: 3 }),
     body("lastName").isLength({ min: 3 }),
-    body("email").isEmail().withMessage("o email precisa ser válido"),
+    body("email").isEmail().isEmpty().withMessage("o email precisa ser válido"),
     body("password")
       .isLength({ min: 8 })
       .withMessage("a senha precisa ter no mínimo 8 caracteres"),
-    body("birthDate").isDate().withMessage("digite uma data válida"),
+    body("birthDate").isDate().isEmpty().withMessage("digite uma data válida"),
     body("zipCode").isNumeric().isLength({ min: 8 }),
   ],
-  usersValidator,
+  validator,
   usersController.create
 );
 router.put(
@@ -34,7 +34,7 @@ router.put(
     body("birthDate").isDate().withMessage("digite uma data válida"),
     body("zipCode").isNumeric().isLength({ min: 8 }),
   ],
-  usersValidator,
+  validator,
   authorization,
   usersController.update
 );
