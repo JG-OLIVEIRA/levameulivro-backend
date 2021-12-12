239b5e2fcb0836099a94ec9345c089fd76a3b587
JG-OLIVEIRA <jorge.gdoliveira8@gmail.com>
2021-12-12 18:47:35 -0300

    adding getBookCoverByISBN function

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ const createError = require("http-errors");
 const express = require("express");
 const cookieParser = require("cookie-parser");
 const logger = require("morgan");
+const cors = requite("cors");
 
 const usersRouter = require("./src/routes/users");
 const booksRouter = require("./src/routes/book");
@@ -13,6 +14,7 @@ app.use(logger("dev"));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
+app.use(cors());
 
 app.use("/users", usersRouter);
 app.use("/books", booksRouter);
---
+++ src/utils/apis/getBookCoverByISBN.js
@@ -0,0 +1,11 @@
+const axios = require("axios");
+
+const api = axios.create({ baseURL: "https://openlibrary.org/api/books" });
+
+module.exports = (ISBN) => {
+  const { data } = await api.get(
+    `https://openlibrary.org/api/books?bibkeys=ISBN:${ISBN}&format=json`
+  );
+
+  return data[`ISBN:${ISBN}`]["thumbnail_url"];
+};
