1495506a69a593acaa640641c236a32a33b99f5b
JG-OLIVEIRA <jorge.gdoliveira8@gmail.com>
2021-12-16 21:42:16 -0300

    fix:sequelize is not defined

--- src/database/models/index.js
+++ src/database/models/index.js
@@ -8,6 +8,7 @@ const env = process.env.NODE_ENV || "development";
 const config = require(__dirname + "/../config/config.js")[env];
 const db = {};
 
+let sequelize;
 if (process.env.DATABASE_URL) {
   sequelize = new Sequelize(process.env.DATABASE_URL, {
     dialectOptions: {
