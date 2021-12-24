58ec60e84ac079571ec1625ef9dce7d6e6ee6b55
JG-OLIVEIRA <jorge.gdoliveira8@gmail.com>
2021-12-24 01:27:44 -0300

    set ISBN to number

--- src/database/models/books.js
+++ src/database/models/books.js
@@ -17,7 +17,7 @@ module.exports = (sequelize, DataTypes) => {
         allowNull: false,
       },
       isbn: {
-        type: DataTypes.STRING,
+        type: DataTypes.INTEGER,
         allowNull: false,
       },
       thumbnail_url: {
