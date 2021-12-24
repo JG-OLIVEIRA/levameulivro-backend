69490feb5ca1736a57b2e0b247707116e5983248
JG-OLIVEIRA <jorge.gdoliveira8@gmail.com>
2021-12-24 01:18:05 -0300

    fix export function

--- src/utils/apis/getBookCoverByISBN.js
+++ src/utils/apis/getBookCoverByISBN.js
@@ -5,8 +5,8 @@ const api = axios.create({
   baseURL: "https://www.googleapis.com/books/v1/volumes",
 });
 
-export async function getBookByISBN(ISBN) {
+module.exports = async (ISBN) => {
   const { data } = await api.get(`?q=isbn:${ISBN}&${process.env.API_KEY}`);
 
   return data["items"][0]["volumeInfo"]["imageLinks"]["thumbnail"];
-}
+};
