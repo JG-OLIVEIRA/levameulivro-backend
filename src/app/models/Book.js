3814c87562195856cb75b0853d41b970ae8471cd
Jorge Oliveira <jorge.gdoliveira8@gmail.com>
2024-05-18 22:02:35 -0300

    created users' tests and refatored code

---
+++ src/app/models/Book.js
@@ -0,0 +1,26 @@
+module.exports = (sequelize, DataTypes) => {
+  const Book = sequelize.define(
+    "Book",
+    {
+      name: DataTypes.STRING,
+      author: DataTypes.STRING,
+      isbn: DataTypes.BIGINT,
+      thumbnail_url: DataTypes.STRING,
+      user_id: DataTypes.INTEGER,
+      description: DataTypes.TEXT
+    },
+  );
+
+  Book.associate = function (models) {
+    Book.hasOne(models.Exchange, {
+      foreignKey: "book_id",
+      as: "exchanges",
+    });
+    Book.belongsTo(models.User, {
+      foreignKey: "user_id",
+      as: "users",
+    });
+  };
+
+  return Book;
+};
