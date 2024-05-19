edb998df043966a49e2da4509d032a104a99a873
Jorge Oliveira <jorge.gdoliveira8@gmail.com>
2024-05-18 23:04:26 -0300

    removing service

--- src/config/database.js
+++ src/config/database.js
@@ -1,8 +1,6 @@
 require("dotenv").config({
     path: process.env.NODE_ENV === "test" ? ".env.test" : ".env"
 });
-
-console.log(process.env.DB_HOST)
   
 module.exports = {
     host: process.env.DB_HOST,
