57024828029f0db40887ac403e2f7ef48616b9c7
JG-OLIVEIRA <jorge.gdoliveira8@gmail.com>
2024-08-18 00:59:22 -0300

    Formaliza o padrão snake_case

--- __tests__/unit/users.test.js
+++ __tests__/unit/users.test.js
@@ -21,6 +21,8 @@ describe("User", () => {
       credit: faker.number.int(100),
     });
 
+    console.log(user)
+
     const compareHash = await bcrypt.compare(user.password, user.password_hash);
 
     expect(compareHash).toBe(true);
