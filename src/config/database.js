7b39c7c8fc7e0f1faee4527422bb5c22bf086456
JG-OLIVEIRA <jorge.gdoliveira8@gmail.com>
2024-08-18 00:38:50 -0300

    Resolvendo problema de SSL

--- src/config/database.js
+++ src/config/database.js
@@ -1,18 +1,24 @@
 require("dotenv").config({
-    path: process.env.NODE_ENV === "test" ? ".env.test" : ".env"
+  path: process.env.NODE_ENV === "test" ? ".env.test" : ".env"
 });
-  
+
 module.exports = {
-    host: process.env.DB_HOST,
-    username: process.env.DB_USER,
-    password: process.env.DB_PASS,
-    database: process.env.DB_NAME,
-    dialect: process.env.DB_DIALECT || "mysql",
-    storage: "./__tests__/database.sqlite",
-    logging: false,
-    define: {
-      timestamps: true,
-      underscored: true,
-      underscoredAll: true
-    }
+  host: process.env.DB_HOST,
+  username: process.env.DB_USER,
+  password: process.env.DB_PASS,
+  database: process.env.DB_NAME,
+  dialect: process.env.DB_DIALECT || "mysql",
+  storage: "./__tests__/database.sqlite",
+  logging: false,
+  dialectOptions: {
+    ssl: {
+      require: true,
+      rejectUnauthorized: false, // Certifique-se de aceitar certificados autoassinados, se for o caso
+    },
+  },
+  define: {
+    timestamps: true,
+    underscored: true,
+    underscoredAll: true
+  }
 };
